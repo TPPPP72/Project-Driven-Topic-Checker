37ede8461317c0102a0f444641807dfd34326fe8
TPPPP <906483498@qq.com>
2025-09-11 15:20:47 +0800

    修改调用checker.exe的入参形式

--- src/checker.cpp
+++ src/checker.cpp
@@ -10,7 +10,7 @@ void Checker::single(int id, int num)
 {
     std::cout << "测试Task" << id << ":" << std::endl;
     Win32::Process process;
-    process.run_cmd(_dir, _dir.get_dir("checker.exe"), {std::format("Task{}_{}", id, num)});
+    process.run_cmd(_dir, _dir.get_dir("checker.exe"), {std::format("{}_{}", id, num)});
     std::vector<std::pair<SafeString, SafeString>> check_path;
     for (int i = 1; i <= num; ++i)
     {
